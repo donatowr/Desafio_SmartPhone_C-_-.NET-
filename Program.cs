0f5b3875a4b258a3abe334b3bf669409fdf531fb
Wellington Donato <dev.donatello3@gmail.com>
2024-01-02 21:51:40 -0300

    Finalizado Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphoneXR = new Iphone("11972395229", "xr-1105", "555-333-444-222", 128);
+iphoneXR.ReceberLigacao();
+iphoneXR.InstalarAplicativo("Uber");
+iphoneXR.Ligar();
+
+Console.WriteLine("-----------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone nokia_1000 = new Nokia("11972395229", "nokia-1105", "666-333-222-333", 256);
+nokia_1000.ReceberLigacao();
+nokia_1000.InstalarAplicativo("Discord");
+nokia_1000.Ligar();
